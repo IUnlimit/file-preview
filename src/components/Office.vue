8721dedd0edb05f8703cd0f2aac3ec1b1c70f4a1
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-02 00:39:34 +0800

    Update: Add office-PDF preview

---
+++ src/components/Office.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="entity.type.toLowerCase() === 'pdf'">
+    <office-pdf v-if="entity.data.length !== 0" :pdf-url="entity.data" />
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import OfficePdf from "./fragment/office-pdf.vue";
+
+const entity = reactive({
+  data: "",
+  type: "pdf",
+});
+
+window["setData"] = (base64, type) => {
+  entity.data = "data:application/pdf;base64," + base64;
+  entity.type = type;
+  return "ok";
+};
+</script>
+
+<style scoped></style>

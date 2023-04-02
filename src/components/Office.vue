db3a3f17d5eab75e89cee59628d0c62892fb7533
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-02 16:19:19 +0800

    Feat: Support more office type

--- src/components/Office.vue
+++ src/components/Office.vue
@@ -1,6 +1,17 @@
 <template>
   <div v-if="entity.type.toLowerCase() === 'pdf'">
-    <office-pdf v-if="entity.data.length !== 0" :pdf-url="entity.data" />
+    <office-pdf
+      v-if="entity.data.length !== 0"
+      :pdf-url="'data:application/pdf;base64,' + entity.data"
+    />
+  </div>
+  <div v-else-if="checkOffice()">
+    <iframe
+      :src="'https://view.officeapps.live.com/op/view.aspx?src=' + entity.data"
+      class="office-online"
+      frameborder="0"
+    >
+    </iframe>
   </div>
 </template>
 
@@ -8,16 +19,31 @@
 import { reactive } from "vue";
 import OfficePdf from "./fragment/office-pdf.vue";
 
+// pdf -> base64 data
+// others -> online url
 const entity = reactive({
   data: "",
   type: "pdf",
 });
 
-window["setData"] = (base64, type) => {
-  entity.data = "data:application/pdf;base64," + base64;
+function checkOffice() {
+  return (
+    ["doc", "docx", "xls", "xlsx", "ppt", "pptx"].indexOf(
+      entity.type.toLowerCase()
+    ) !== -1
+  );
+}
+
+window["setData"] = (data, type) => {
+  entity.data = data;
   entity.type = type;
   return "ok";
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.office-online {
+  height: 100vh;
+  width: 100%;
+}
+</style>

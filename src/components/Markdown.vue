9b03795bfbc376d11aa6d8df080480af5938450a
IllTamer <78360471+IllTamer@users.noreply.github.com>
2023-04-02 16:18:55 +0800

    Feat: Add preview-markdown

---
+++ src/components/Markdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <preview-markdown
+      v-if="entity.data.length !== 0"
+      :markdown="entity.data"
+    ></preview-markdown>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import PreviewMarkdown from "./fragment/preview-markdown.vue";
+
+const entity = reactive({
+  data: "",
+});
+
+window["setData"] = (data) => {
+  entity.data = data;
+  return "ok";
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/fragment/preview-markdown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-html="content"></div>
+</template>
+
+<script setup lang="ts">
+import { computed, nextTick, onActivated, onMounted, ref } from "vue";
+import { marked } from "marked";
+import Prism from "prismjs";
+import "../../../public/css/redefine-light.css";
+
+const props = defineProps({
+  markdown: {
+    type: String,
+    required: true,
+  },
+});
+
+const content = computed(() => marked(props.markdown));
+
+onMounted(() => Prism.highlightAll());
+</script>
+
+<style scoped></style>
